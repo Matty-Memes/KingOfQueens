664070f23842272440171dec2c75571152b19fe5
brian <bharmon01@qub.ac.uk>
2017-03-27 12:53:12 +0100

    refactored some of the monsterCard class constructor. began work on the playspace to create some of the required objects.

---
+++ app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/oldCode/MattsOldCode/MonsterCardOldConstructor.java
@@ -0,0 +1,124 @@
+package javalampstudos.kingofqueens.kingOfQueens.oldCode.MattsOldCode;
+
+/**
+ * Created by brian on 27/03/2017.
+ */
+
+public class MonsterCardOldConstructor {
+
+
+
+    // matts old constructor for Monster card, 27/03/2017
+
+    /*
+    import javalampstudos.kingofqueens.kingOfQueens.objects.Cards.BasicCard;
+import javalampstudos.kingofqueens.kingOfQueens.objects.Cards.CardLevel;
+import javalampstudos.kingofqueens.kingOfQueens.objects.Cards.ManaTypes;
+import javalampstudos.kingofqueens.kingOfQueens.objects.graveYard;
+public class MonsterCard extends BasicCard {
+    private CardLevel level; // ENUM
+    private int health;
+    private int defence;
+    private CardSchools strength;
+    private CardSchools weakness; // ENUM
+    // Attack Values
+    private String attack1Name;
+    private int attack1Strength;
+    // holds a list of the mana types required
+    private ManaTypes [] attack1ManaRequired;
+    private String attack2Name;
+    private int attack2Strength;
+    // holds a list of the mana types required
+    private ManaTypes [] attack2ManaRequired;
+    // Reduce the number of fields somehow
+    public MonsterCard(int x, int y, int width, int height, Bitmap Sprite,Bitmap cardBackSprite, boolean destroyed,int pointerID,
+                       String name, String description, CardLevel level, int health,int defence, CardSchools cardSchools, CardSchools strength, CardSchools weakness,
+                       String attack1Name, int attack1Strength, ManaTypes [] attack1ManaRequired,
+                       String attack2Name, int attack2Strength, ManaTypes [] attack2ManaRequired ) {
+        super(x, y, width, height, Sprite,cardBackSprite, name,description,cardSchools,destroyed,pointerID);
+        this.level = level;
+        this.health = health;
+        this.defence = defence;
+        this.strength = strength;
+        this.weakness = weakness;
+        this.attack1Name = attack1Name;
+        this.attack1Strength = attack1Strength;
+        this.attack1ManaRequired = attack1ManaRequired;
+        this.attack2Name = attack2Name;
+        this.attack2Strength = attack2Strength;
+        this.attack2ManaRequired = attack2ManaRequired;
+    }
+    // ADD GETTERS AND SETTERS // Brian :: Matt will you keep all these getters and setters i need to accsess them.
+    public CardLevel getLevel() {
+        return level;
+    }
+    public void setLevel(CardLevel level) {
+        this.level = level;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public int getDefence() {
+        return defence;
+    }
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+    public CardSchools getStrength() {
+        return strength;
+    }
+    public void setStrength(CardSchools strength) {
+        this.strength = strength;
+    }
+    public CardSchools getWeakness() {
+        return weakness;
+    }
+    public void setWeakness(CardSchools weakness) {
+        this.weakness = weakness;
+    }
+    public String getAttack1Name() {
+        return attack1Name;
+    }
+    public void setAttack1Name(String attack1Name) {
+        this.attack1Name = attack1Name;
+    }
+    public int getAttack1Strength() {
+        return attack1Strength;
+    }
+    public void setAttack1Strength(int attack1Strength) {
+        this.attack1Strength = attack1Strength;
+    }
+    public ManaTypes[] getAttack1ManaRequired() {
+        return attack1ManaRequired;
+    }
+    public void setAttack1ManaRequired(ManaTypes[] attack1ManaRequired) {
+        this.attack1ManaRequired = attack1ManaRequired;
+    }
+    public String getAttack2Name() {
+        return attack2Name;
+    }
+    public void setAttack2Name(String attack2Name) {
+        this.attack2Name = attack2Name;
+    }
+    public int getAttack2Strength() {
+        return attack2Strength;
+    }
+    public void setAttack2Strength(int attack2Strength) {
+        this.attack2Strength = attack2Strength;
+    }
+    public ManaTypes[] getAttack2ManaRequired() {
+        return attack2ManaRequired;
+    }
+    public void setAttack2ManaRequired(ManaTypes[] attack2ManaRequired) {
+        this.attack2ManaRequired = attack2ManaRequired;
+    }
+
+
+
+
+
+     */
+}
