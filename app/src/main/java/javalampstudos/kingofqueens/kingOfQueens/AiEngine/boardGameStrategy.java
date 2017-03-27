6269ed7d0cc7612e05a3e9bc89f029e12497a55a
brian <bharmon01@qub.ac.uk>
2017-03-27 15:44:06 +0100

    worked on ai logic, fixed some of the bugs in the find best card method

--- app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/AiEngine/boardGameStrategy.java
+++ app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/AiEngine/boardGameStrategy.java
@@ -13,7 +13,7 @@ import javalampstudos.kingofqueens.kingOfQueens.objects.GameBoard.Hand;
 public class boardGameStrategy {
 
 
-    // PlaySpace AiPlayer = new PlaySpace(); // still neeeds to be populated
+    PlaySpace AiPlayer = new PlaySpace(); // still neeeds to be populated
 
 
 /* BRIAN : COMMENTED OUT DUE TO ERROES
@@ -57,37 +57,41 @@ public class boardGameStrategy {
 
 // 40111707 brians method
     // this method will find the highest attack card that can be played.
-
-
-
+    // in order for this method to be finished manacounter needs to be made into a hashmap.
     public void searchHandForCardWithHighestAttack(Hand hand, MonsterCard enemeyCard)
     {
+
         int bestCardIndex=0;
         // for loop begins at one so that first card can be compared to
-        for (int i=1; i < hand.length; i++)
+        for (int i=1; i < hand.getHand().length; i++)
         {// within here i need to make sure only monster cards will be accepted.
             // this if statement checks 3 things.
             //1. the card is higher attack than enemeys defence
             // 2.the card is also higher than the best card so far
             // 3.the cards mana also allows it to be played.
-            if (hand.ReturnCard(i).getAttackValue() > enemeyCard.getDefence()
-                    && hand[i].getAttack1Strength() > hand[bestCardIndex].getAttack1Strength()
-                    && hand[i].getAttack1ManaRequired() < ManaCounter.get(hand[i].getCardSchool())) // manacounter still needs to be changed to a hashmap
+
+            if(hand.ReturnCard(i) instanceof MonsterCard)
+            {
+
+            if (((MonsterCard) hand.ReturnCard(i)).getAttackValue() > enemeyCard.getDefence()
+                    && ((MonsterCard) hand.ReturnCard(i)).getAttackValue() > ((MonsterCard) hand.ReturnCard(bestCardIndex)).getAttackValue()
+                    && ((MonsterCard) hand.ReturnCard(i)).getAttackValue() < ManaCounter.get(((MonsterCard) hand.ReturnCard(i)).getCardSchool())) // manacounter still needs to be changed to a hashmap
             {
                 bestCardIndex =i;
             }
             // this assumes the card that has the highest attack cannot be played,
 //				so it simply checks for a card with a better attack than the player, with enough mana to be played.
-            else if (hand[i].getAttack1Strength() > enemeyCard.getDefence()
-                    &&  hand[i].getAttack1ManaRequired() < ManaCounter.get(hand[i].getCardSchool())
+            else if (((MonsterCard) hand.ReturnCard(i)).getAttackValue() > enemeyCard.getDefence()
+                    &&  ((MonsterCard) hand.ReturnCard(i)).getAttackManaRequired() <  ManaCounter.get(((MonsterCard) hand.ReturnCard(i)).getCardSchool()))
             {
                 bestCardIndex = i;
             }
 
             playCard(hand[bestCardIndex]);
         }
+        }
 }
-*/
+
 
 // brians method,
     // this method will choose which zone to play the card on then drag it to that zone.
