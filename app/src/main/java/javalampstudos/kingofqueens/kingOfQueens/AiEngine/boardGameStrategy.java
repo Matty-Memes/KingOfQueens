beba00751972d4ae826c76f6dbe0f1d206d2843f
brian <bharmon01@qub.ac.uk>
2017-03-27 16:03:49 +0100

    worked on ai logic, fixed some of the bugs in playcard method, making use of the Zones on the board to send the cards to.

--- app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/AiEngine/boardGameStrategy.java
+++ app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/AiEngine/boardGameStrategy.java
@@ -13,47 +13,7 @@ import javalampstudos.kingofqueens.kingOfQueens.objects.GameBoard.Hand;
 public class boardGameStrategy {
 
 
-    PlaySpace AiPlayer = new PlaySpace(); // still neeeds to be populated
-
-
-/* BRIAN : COMMENTED OUT DUE TO ERROES
-    //this method is for allocating the monster cards to the correct hash map.
-    public void allocateMonsterCardToCorectMapArea(deck Aideck)
-    {// only checking the monster cards here could then thero itly just use 20 as a final,
-        // after the sorft method makes sure the monsters are all of the first 20 cards in order of atack.
-        int i =0;
-        while(i < Aideck.size ())        {
-            if(Aideck.get(i).getAttack() > 50)
-            {
-                if(Aideck.get(i).getDefence() > 50)
-                {
-                    //insert into map
-                    map.put(cardPowerLevels.HIGHATTACK_HIGHDEFENCE,Aideck.get(i));
-                    i++;
-                }
-                else {
-                    map.put(cardPowerLevels.HIGHATACK_LOWDEFENCE,Aideck.get(i));
-                    i++;
-                }
-
-
-            }
-            if(Aideck.get(i).getAttack() < 50)
-            {
-                if(Aideck.get(i).getDefence() < 50)
-                {
-                    //insert into map
-                    map.put(cardPowerLevels.LOWATTACK_LOWDEFENCE,Aideck.get(i);
-                    i++;
-                }
-                else {
-                    map.put(cardPowerLevels.LOWATTACK_HIGHDEFENCE,Aideck.get(i));
-                    i++;
-                }
-            }
-        }
-    */
-
+   PlaySpace AiPlayer = new PlaySpace(); // still neeeds to be populated
 
 // 40111707 brians method
     // this method will find the highest attack card that can be played.
@@ -87,7 +47,7 @@ public class boardGameStrategy {
                 bestCardIndex = i;
             }
 
-            playCard(hand[bestCardIndex]);
+            playCard(((MonsterCard) hand.ReturnCard(bestCardIndex)));
         }
         }
 }
@@ -96,19 +56,19 @@ public class boardGameStrategy {
 // brians method,
     // this method will choose which zone to play the card on then drag it to that zone.
     //40111707
-    public void playCard(basicCard card) {
+    public void playCard(MonsterCard card) {
 
 // you need to allocate a zone for the card to be sent to
-// waiting for matt to change the zone stuff
-        if (!zone1.isActive()) {
+// requires a seek method, it should send the card to the correct zone, then also give the card zone that card as its current card.
+        if (AiPlayer.getZoneLeft().isActive() == false) {
             //call the seek method to send the card there
-            card.seek(zone1.position());
-        } else if (!zone2.isActive()) {
+            card.seek(AiPlayer.getZoneLeft().setCurrentCard(card));
+        } else if (AiPlayer.getZoneMiddle().isActive() == false) {
             //call the seek method to send the card there
-            card.seek(zone2.position());
-        } else if (!zone3.isActive()) {
+            card.seek(AiPlayer.getZoneMiddle().setCurrentCard(card));
+        } else if (AiPlayer.getZoneRight().isActive() == false) {
             //call the seek method to send the card there
-            card.seek(zone3.position());
+            card.seek(AiPlayer.getZoneRight().setCurrentCard(card));
         } else
         {
             // the card cannot be played - there is no room.
