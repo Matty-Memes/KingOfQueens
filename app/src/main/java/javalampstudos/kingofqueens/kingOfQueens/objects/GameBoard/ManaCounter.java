625241e9ebf53a52464adaa0a3be53fc7ca42be0
brian <bharmon01@qub.ac.uk>
2017-03-30 14:19:44 +0100

    created my version of Manacounter with HashMap implementation - TBC if this option will remain within the assignment.

--- app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/objects/GameBoard/ManaCounter.java
+++ app/src/main/java/javalampstudos/kingofqueens/kingOfQueens/objects/GameBoard/ManaCounter.java
@@ -12,11 +12,11 @@ public class ManaCounter
 {
 
 
-        // augmented by brian on 28/03/2017 see oldManaCounterConstructorClass
+
         private HashMap<ManaTypes,Integer> unusedMana;
          private HashMap<ManaTypes, Integer> manaCounterHashMap;
 
-        // hashmap is created within the constructor, this allows for it to be populated with the correct manatypes.
+        // hashmaps are created within the constructor, this allows for it to be populated with the correct manatypes.
         public ManaCounter() {
              manaCounterHashMap = new HashMap<ManaTypes, Integer>();
             setupManaCounter(manaCounterHashMap);
@@ -52,28 +52,26 @@ public class ManaCounter
             map.put(ManaTypes.SOCIAL_SCIENCES_MANA, 0);
         }
 
-        // 40111707
-        // brians method
-        // this method returns the value from the hashmap, by using the enum key for it.
-        public void getManaCountValue(ManaTypes manaType) {
-            getManaCounterHashMap().get(manaType);
-        }
-
-
-
-        //Add mana to the mana counter
+           //Add mana to the mana counter
         // brian method
         public void addMana(ManaTypes mana, int amount) {
             manaCounterHashMap.put(mana, amount);
         }
 
+        // 40111707
+        // brians method
         //Use mana from the pool in for an attack
-        public void useMana(int attackCost,ManaTypes mana) {
-           unusedMana.put(mana,manaCounterHashMap.get(mana)-attackCost);
+        public void useMana(int manaCost,ManaTypes mana) {
+            if((manaCounterHashMap.get(mana)-manaCost) > 0){
+                unusedMana.put(mana, manaCounterHashMap.get(mana) - manaCost);
+            }
+            else{
+                // there is not enough mana for this attack
+            }
         }
         // 40111707
         // brians method
-        //Reset the mana at the beginning of every turn
+        //Reset the unusedMana at the beginning of every turn
         public void resetMana() {
             setupManaCounter(unusedMana);
         }
